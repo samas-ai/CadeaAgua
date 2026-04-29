625da39224a989dc70967594af1331e920acbb5d
Samuel <145884210+samas-ai@users.noreply.github.com>
2026-04-29 08:17:59 -0300

    Delete CadeaAgua-main directory

--- CadeaAgua-main/src/main/java/br/com/cadeaagua/api/repository/RegiaoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.cadeaagua.api.repository;
-
-import br.com.cadeaagua.api.entity.RegiaoAbastecimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegiaoRepository extends JpaRepository<RegiaoAbastecimento, Integer> {
-    RegiaoAbastecimento findByBairro(String bairro);
-}
